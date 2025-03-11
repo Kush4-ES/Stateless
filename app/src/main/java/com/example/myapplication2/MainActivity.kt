bdc88c0a00e683daf01681fff995e9b70d1ec984
khushbir.kumar.7e8 <khushbir.kumar.7e8@itb.cat>
2025-03-11 13:35:35 +0100

    k

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.myapplication2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.myapplication2.ui.theme.MyApplication2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplication2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Text(
+        text = "Bye $name!",
+        modifier = modifier.padding(50.dp).alpha(10.0f)
+    )
+    ConstraintLayout(modifier.fillMaxSize()) {
+        val (boxRed, boxYellow, boxGreen, boxCyan, boxMagenta) = createRefs()
+        Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Yellow).constrainAs(boxYellow){
+            start.linkTo(boxRed.start)
+            bottom.linkTo(boxRed.top)
+
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Green).constrainAs(boxGreen){
+            start.linkTo(boxRed.start)
+            top.linkTo(boxRed.bottom)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Cyan).constrainAs(boxCyan){
+            end.linkTo(boxRed.start)
+            top.linkTo(boxRed.top)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            start.linkTo(boxRed.end)
+            top.linkTo(boxRed.top)
+        })
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    MyApplication2Theme {
+        Greeting("Android")
+    }
+
+}
